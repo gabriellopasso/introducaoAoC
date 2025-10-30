3f549391dde2baa2909d8b1fce53ca7cdc3442d7
gabriellopasso <gabriellopasso@gmail.com>
2025-10-30 20:47:24 -0300

    Final desfio nivel basico

--- superTrunfo1/main.c
+++ superTrunfo1/main.c
@@ -9,19 +9,25 @@ int main() {
     char nomeCidade[50];
     char estado;
 
-    printf("Digite o estado em que a cidade se encontra(Uma letra de 'a' a 'h'): ");
-    scanf("%s", &estado);
+    // Variáveis da cidade 2
+    int codigo2, pop2, pontos2;
+    float area2, pib2;
+    char nome2[50];
+    char estado2;
 
-    printf("Coloque o código da carta:  ");
+    printf("Digite o estado em que a cidade se encontra(Uma letra de 'a' a 'h'): ");
+    scanf("%c", &estado);
+
+    printf("Coloque o codigo da carta:  ");
     scanf("%d", &codigoCarta);
     
     printf("Digite o nome da cidade: ");
     scanf("%s", &nomeCidade);
 
-    printf("Diga a população da cidade: ");
+    printf("Diga a populacao da cidade: ");
     scanf("%d", &populacao);
     
-    printf("Informe a area da cidade (m²): ");
+    printf("Informe a area da cidade (m2): ");
     scanf("%f", &area);
 
     printf("Qual o PIB da cidade?: ");
@@ -30,15 +36,38 @@ int main() {
     printf("A quantidade de pontos turisticos:  ");
     scanf("%d", &pontosTuristicosN);
 
+// --- Cidade 2 ---
+    printf("\n=== Cadastro da Cidade 2 ===\n");
+    printf("Estado (letra de 'a' a 'h'): ");
+    scanf(" %c", &estado2);
+
+    printf("Codigo da carta: ");
+    scanf("%d", &codigo2);
+    
+    printf("Nome da cidade: ");
+    scanf("%49s", nome2);
+
+    printf("Populacao: ");
+    scanf("%d", &pop2);
+    
+    printf("Area (m2): ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos2);
+
     printf(
     "Carta 1:\n"
-    "Estado: %s\n"
-    "Código: %d\n"
+    "Estado: %c\n"
+    "Codigo: %d\n"
     "Nome da Cidade: %s\n"
-    "População: %d\n"
-    "Área: %.2f\n"
+    "Populacao: %d\n"
+    "Area: %.2f\n"
     "PIB: %.2f\n"
-    "Número de Pontos Turísticos: %d\n",
+    "Numero de Pontos Turísticos: %d\n",
     estado,
     codigoCarta,
     nomeCidade,
@@ -46,7 +75,10 @@ int main() {
     area,
     pib,
     pontosTuristicosN
-);
+    );
 
+    printf("\n=== CIDADE 2 ===\n");
+    printf("Estado: %c\nCodigo: %d\nNome: %s\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\nPontos turísticos: %d\n",
+           estado2, codigo2, nome2, pop2, area2, pib2, pontos2);
 
 }
